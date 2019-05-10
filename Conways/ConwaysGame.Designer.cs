17e25a4a19bf463f8d1826eaea3f3df6ba03e993
MichaelBaldwin <49702481+MichaelManiac@users.noreply.github.com>
2019-05-10 05:13:21 -0400

    Add project files.

---
+++ Conways/ConwaysGame.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Conways
+{
+    partial class ConwaysGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.GenerationTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.msToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.msToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.FillSquare_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 976);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SquareGrid_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SquareGrid_MouseUp);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.EmptySquare_Tick);
+            // 
+            // GenerationTimer
+            // 
+            this.GenerationTimer.Tick += new System.EventHandler(this.Generation_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.setSpeedToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // setSpeedToolStripMenuItem
+            // 
+            this.setSpeedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msToolStripMenuItem,
+            this.msToolStripMenuItem1,
+            this.msToolStripMenuItem2,
+            this.msToolStripMenuItem3});
+            this.setSpeedToolStripMenuItem.Name = "setSpeedToolStripMenuItem";
+            this.setSpeedToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setSpeedToolStripMenuItem.Text = "Set Speed";
+            this.setSpeedToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.setSpeedToolStripMenuItem_DropDownItemClicked);
+            // 
+            // msToolStripMenuItem
+            // 
+            this.msToolStripMenuItem.Name = "msToolStripMenuItem";
+            this.msToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.msToolStripMenuItem.Text = "10ms";
+            // 
+            // msToolStripMenuItem1
+            // 
+            this.msToolStripMenuItem1.Name = "msToolStripMenuItem1";
+            this.msToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.msToolStripMenuItem1.Text = "50ms";
+            // 
+            // msToolStripMenuItem2
+            // 
+            this.msToolStripMenuItem2.Checked = true;
+            this.msToolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.msToolStripMenuItem2.Name = "msToolStripMenuItem2";
+            this.msToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.msToolStripMenuItem2.Text = "100ms";
+            // 
+            // msToolStripMenuItem3
+            // 
+            this.msToolStripMenuItem3.Name = "msToolStripMenuItem3";
+            this.msToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.msToolStripMenuItem3.Text = "500ms";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter: Start / Stop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(758, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Left Click: Fill Square";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(870, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Right Click: Empty Square";
+            // 
+            // ConwaysGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 1000);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ConwaysGame";
+            this.Text = "Conway\'s Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer GenerationTimer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem msToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem msToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem msToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem msToolStripMenuItem3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Conways/ConwaysGame.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Conways
+{
+    public partial class ConwaysGame : Form
+    {
+        public static Bitmap Map;
+        public static Color CurrentColor = Color.Black;
+        public bool IsStarted = false;
+        public Square[,] SquareArray;
+
+        public ConwaysGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SquareArray = new Square[pictureBox1.Width / 10, pictureBox1.Height / 10];
+            Map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            for (var i = 0; i < SquareArray.GetLength(0); i++)
+            for (var j = 0; j < SquareArray.GetLength(1); j++)
+            {
+                SquareArray[i, j] = new Square(new Point(i * 10, j * 10));
+                SquareArray[i, j].EmptySquare();
+            }
+
+            UpdateGraphics();
+        }
+
+        public void UpdateSquares()
+        {
+            for (var i = 0; i < SquareArray.GetLength(0); i++)
+            for (var j = 0; j < SquareArray.GetLength(1); j++)
+                if (SquareArray[i, j].IsAlive && SquareArray[i, j].SetForChange)
+                {
+                    SquareArray[i, j].EmptySquare();
+                    SquareArray[i, j].SetForChange = false;
+                    SquareArray[i, j].IsAlive = false;
+                }
+                else if (!SquareArray[i, j].IsAlive && SquareArray[i, j].SetForChange)
+                {
+                    SquareArray[i, j].FillSquare(CurrentColor);
+                    SquareArray[i, j].SetForChange = false;
+                    SquareArray[i, j].IsAlive = true;
+                }
+
+            UpdateGraphics();
+        }
+
+        public void UpdateGraphics()
+        {
+            pictureBox1.Image = Map;
+        }
+
+        public void ReColorAll()
+        {
+            for (var i = 0; i < SquareArray.GetLength(0); i++)
+            for (var j = 0; j < SquareArray.GetLength(1); j++)
+                if (SquareArray[i, j].IsFilled)
+                    SquareArray[i, j].FillSquare(CurrentColor);
+            UpdateGraphics();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                if (GenerationTimer.Enabled)
+                {
+                    CurrentColor = Color.Black;
+                    GenerationTimer.Enabled = false;
+                    setSpeedToolStripMenuItem.Enabled = true;
+                    GenerationTimer.Stop();
+                    ReColorAll();
+                }
+                else
+                {
+                    CurrentColor = Color.Blue;
+                    GenerationTimer.Enabled = true;
+                    setSpeedToolStripMenuItem.Enabled = false;
+                    GenerationTimer.Start();
+                    ReColorAll();
+                }
+            }
+        }
+
+        public void NextGeneration()
+        {
+            for (var i = 0; i < SquareArray.GetLength(0); i++)
+            for (var j = 0; j < SquareArray.GetLength(1); j++)
+            {
+                var currentSquare = SquareArray[i, j];
+                var count = GetNeighborCount(i, j);
+                if (currentSquare.IsAlive)
+                {
+                    if (count < 2 || count > 3)
+                        currentSquare.SetForChange = true;
+                }
+                else if (count == 3)
+                {
+                    currentSquare.SetForChange = true;
+                }
+            }
+
+            UpdateSquares();
+        }
+
+        private int GetNeighborCount(int i, int j)
+        {
+            var count = 0;
+            if (i != 0)
+                if (SquareArray[i - 1, j].IsAlive)
+                    count++;
+            if (j != 0)
+                if (SquareArray[i, j - 1].IsAlive)
+                    count++;
+            if (j != 0 && i != 0)
+                if (SquareArray[i - 1, j - 1].IsAlive)
+                    count++;
+            if (j != SquareArray.GetLength(1) - 1)
+                if (SquareArray[i, j + 1].IsAlive)
+                    count++;
+            if (i != SquareArray.GetLength(0) - 1)
+                if (SquareArray[i + 1, j].IsAlive)
+                    count++;
+            if (i != SquareArray.GetLength(0) - 1 && j != SquareArray.GetLength(1) - 1)
+                if (SquareArray[i + 1, j + 1].IsAlive)
+                    count++;
+            if (i != 0 && j != SquareArray.GetLength(1) - 1)
+                if (SquareArray[i - 1, j + 1].IsAlive)
+                    count++;
+            if (j != 0 && i != SquareArray.GetLength(0) - 1)
+                if (SquareArray[i + 1, j - 1].IsAlive)
+                    count++;
+            return count;
+        }
+
+        private void FillSquare_Tick(object sender, EventArgs e)
+        {
+            if (IsStarted || !pictureBox1.ClientRectangle.Contains(PointToClient(MousePosition)) ||
+                GenerationTimer.Enabled) return;
+            var position = pictureBox1.PointToClient(Cursor.Position);
+            var currentSquare = SquareArray[(position.X - position.X % 10) / 10,
+                (position.Y - position.Y % 10) / 10];
+            if (!currentSquare.IsAlive)
+            {
+                currentSquare.IsAlive = true;
+                currentSquare.FillSquare(CurrentColor);
+            }
+
+            UpdateGraphics();
+        }
+
+        private void EmptySquare_Tick(object sender, EventArgs e)
+        {
+            if (IsStarted || !pictureBox1.ClientRectangle.Contains(PointToClient(MousePosition)) ||
+                GenerationTimer.Enabled) return;
+            var position = pictureBox1.PointToClient(Cursor.Position);
+            var currentSquare = SquareArray[(position.X - position.X % 10) / 10,
+                (position.Y - position.Y % 10) / 10];
+            if (currentSquare.IsAlive)
+            {
+                currentSquare.IsAlive = false;
+                currentSquare.EmptySquare();
+            }
+
+            UpdateGraphics();
+        }
+
+        private void Generation_Tick(object sender, EventArgs e)
+        {
+            NextGeneration();
+        }
+
+        private void SquareGrid_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            else
+            {
+                timer2.Enabled = true;
+                timer2.Start();
+            }
+        }
+
+        private void SquareGrid_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                timer1.Stop();
+            else
+                timer2.Stop();
+        }
+
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < SquareArray.GetLength(0); i++)
+            for (var j = 0; j < SquareArray.GetLength(1); j++)
+                if (SquareArray[i, j].IsAlive)
+                {
+                    SquareArray[i, j].IsAlive = false;
+                    SquareArray[i, j].EmptySquare();
+                }
+
+            UpdateGraphics();
+        }
+
+        private void setSpeedToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem == msToolStripMenuItem)
+            {
+                msToolStripMenuItem.Checked = true;
+                msToolStripMenuItem1.Checked = false;
+                msToolStripMenuItem2.Checked = false;
+                msToolStripMenuItem3.Checked = false;
+                GenerationTimer.Interval = 10;
+            }
+
+            if (e.ClickedItem == msToolStripMenuItem1)
+            {
+                msToolStripMenuItem.Checked = false;
+                msToolStripMenuItem1.Checked = true;
+                msToolStripMenuItem2.Checked = false;
+                msToolStripMenuItem3.Checked = false;
+                GenerationTimer.Interval = 50;
+            }
+
+            if (e.ClickedItem == msToolStripMenuItem2)
+            {
+                msToolStripMenuItem.Checked = false;
+                msToolStripMenuItem1.Checked = false;
+                msToolStripMenuItem2.Checked = true;
+                msToolStripMenuItem3.Checked = false;
+                GenerationTimer.Interval = 100;
+            }
+
+            if (e.ClickedItem == msToolStripMenuItem3)
+            {
+                msToolStripMenuItem.Checked = false;
+                msToolStripMenuItem1.Checked = false;
+                msToolStripMenuItem2.Checked = false;
+                msToolStripMenuItem3.Checked = true;
+                GenerationTimer.Interval = 500;
+            }
+        }
+
+        public class Square
+        {
+            private readonly Point _endPoint;
+            private readonly Point _startPoint;
+            public bool IsAlive;
+            public bool IsFilled;
+            public bool SetForChange;
+
+            public Square(Point startPoint)
+            {
+                _startPoint = startPoint;
+                _endPoint = new Point(startPoint.X + 10, startPoint.Y + 10);
+            }
+
+            public void FillSquare(Color color)
+            {
+                for (var i = _startPoint.X; i < _endPoint.X - 1; i++)
+                for (var j = _startPoint.Y; j < _endPoint.Y - 1; j++)
+                    Map.SetPixel(i, j, color);
+                IsFilled = true;
+            }
+
+            public void EmptySquare()
+            {
+                FillSquare(Color.White);
+
+                using (var g = Graphics.FromImage(Map))
+                {
+                    using (var pen = new Pen(Color.Black, 1))
+                    {
+                        g.DrawRectangle(pen, _startPoint.X, _startPoint.Y, _endPoint.X, _endPoint.Y);
+                    }
+                }
+
+                IsFilled = false;
+            }
+        }
+    }
+}
